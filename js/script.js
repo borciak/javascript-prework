b3a08d42bbf99591e4eefbb4a7cebfeb1e69e588
Kasia Janiszewska <katarzyna.janiszewska@aviva.pl>
2019-06-12 19:37:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,16 +16,16 @@ function buttonClicked(argButtonName) {
   computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);}
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  displayResult(playerMove, computerMove)}
-  function getMoveName(argMoveId) {console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-    if (argMoveId == 1) {return 'kamień';
-    } else if (argMoveId == 2) {return 'papier';
-    } else if (argMoveId == 3) {return 'nożyce';
-    } else {printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-      return 'kamień';  }}
-function displayResult(argPlayerMove, argComputerMove) 
+  function displayResult(argPlayerMove, argComputerMove) 
   {console.log ('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {printMessage('Wygrywasz!');} 
   else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {printMessage('Wygrywasz!');}
   else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {printMessage('Wygrywasz!');} 
-  else {printMessage('Przegrywasz'); }}
+  else {printMessage('Przegrywasz'); }
+  function getMoveName(argMoveId) {console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {return 'kamień';
+  } else if (argMoveId == 2) {return 'papier';
+  } else if (argMoveId == 3) {return 'nożyce';
+  } else {printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'kamień';  }
+}
